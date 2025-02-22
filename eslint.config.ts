e96f542ac2cd92c29e7916011fc2013c08f319df
a.liulko <a.liulko@avax.de>
2025-02-23 00:21:01 +0100

    add useful eslint rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+ 
+import pluginVue from 'eslint-plugin-vue';
+import {defineConfigWithVueTs, vueTsConfigs} from '@vue/eslint-config-typescript';
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -18,13 +18,49 @@ export default defineConfigWithVueTs(
         ignores: ['**/dist/**', '**/out/**', '**/.vite/**', '**/dist-ssr/**', '**/coverage/**'],
     },
 
-    pluginVue.configs['flat/essential'],
+    pluginVue.configs['flat/recommended'],
     vueTsConfigs.all,
 
     {
         rules: {
+            'vue/component-name-in-template-casing': ['error', 'kebab-case', {
+                "registeredComponentsOnly": false,
+                "ignores": [],
+            }],
             '@typescript-eslint/no-magic-numbers': 'warn',
             '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+            'semi': ["error", "always"],
+            "comma-dangle": ["error", {
+                "arrays": "never",
+                "objects": "always",
+                "imports": "never",
+                "exports": "always",
+                "functions": "never",
+            }],
+            "@typescript-eslint/no-duplicate-enum-values": "error",
+            "@typescript-eslint/prefer-enum-initializers": "error",
+            "@typescript-eslint/naming-convention": [
+                "error",
+                {
+                    "selector": "interface",
+                    "prefix": ["I"],
+                    "format": ["PascalCase"],
+                },
+                {
+                    "selector": "enum",
+                    "format": ["PascalCase"],
+                    "prefix": ["I"],
+                },
+                {
+                    "selector": "typeAlias",
+                    "format": ["PascalCase"],
+                    // "prefix": ["I"],
+                },
+                {
+                    "selector": "enumMember",
+                    "format": ["UPPER_CASE"],
+                }
+            ],
         },
     }
-)
+);
